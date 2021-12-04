bb7ee7643649307179299d92ddbf3eeeae6149b3
Anton Losman <87125898+antlosman@users.noreply.github.com>
2021-12-04 11:47:11 +0200

    test method deleted

--- pratical_project_hibernate-main/src/main/java/persistence/RepositoryOwner.java
+++ pratical_project_hibernate-main/src/main/java/persistence/RepositoryOwner.java
@@ -46,9 +46,7 @@ public class RepositoryOwner {
             entityManager.getTransaction().rollback();
         }
     }
-    public void test() {
-        
-    }
+   
 
     public List<Owner> ownerList() {
         List<Owner> allOwners = null;
